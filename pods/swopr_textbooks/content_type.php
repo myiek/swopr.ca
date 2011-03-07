35366f5a1940e6001de0211e52562f8f7ad5599f
Michael Chu <mchchu88@gmail.com>
2011-03-07 02:32:50 -0500

    Added the textbook content, created a basic input form for textbooks

---
+++ pods/swopr_textbooks/content_type.php
@@ -0,0 +1,13 @@
+<? 
+
+
+	$content_type ='textbooks';
+	$permalink = 'textbook';
+	$edit_link = "textbooks";
+	$input_template = 'swopr_textbook_input';
+	$output_template = 'swopr_textbook_output';
+	$pod_dir = "swopr_textbooks";
+
+
+
+?>
